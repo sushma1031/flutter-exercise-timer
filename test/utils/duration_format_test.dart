6ec659b69152f82b666b179f60c521d84a4796b6
sushma1031 <sushmajram@gmail.com>
2024-07-30 16:53:10 +0530

    refactor: group utility tests

--- test/utils/duration_format_test.dart
+++ test/utils/duration_format_test.dart
@@ -2,10 +2,12 @@ import 'package:test/test.dart';
 import 'package:exercise_timer/utils/duration_format.dart';
 
 void main() {
-  test('.formatDuration() formats duration correctly', () {
-    var d = Duration(seconds: 60);
-    expect(formatDuration(d), equals('01 : 00'));
-    d = Duration(seconds: 45);
-    expect(formatDuration(d), equals('45'));
+  group('Utility functions work correctly', () {
+    test('.formatDuration() formats duration correctly', () {
+      var d = Duration(seconds: 60);
+      expect(formatDuration(d), equals('01 : 00'));
+      d = Duration(seconds: 45);
+      expect(formatDuration(d), equals('45'));
+    });
   });
 }
