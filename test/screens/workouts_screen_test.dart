bfc2d9e6d5ff03a735b1e7099a9acc499d6c0e22
sushma1031 <sushmajram@gmail.com>
2024-08-04 10:49:58 +0530

    refactor: update screen tests

--- test/screens/workouts_screen_test.dart
+++ test/screens/workouts_screen_test.dart
@@ -4,7 +4,7 @@ import '../services/mock_storage_service.dart';
 import 'package:exercise_timer/main.dart';
 
 void main() {
-  var app = MyApp(db: MockStorageService());
+  var app = MyApp(db: MockStorageService('workoutBox'));
   group('Workouts Screen loads correctly', () {
     testWidgets('show "Loading..." when loading workouts', (tester) async {
       await tester.pumpWidget(app);
@@ -17,9 +17,8 @@ void main() {
       expect(find.text('Loading...'), findsNothing);
       expect(find.text('Abs'), findsOneWidget);
       expect(find.text('Thighs'), findsOneWidget);
-      expect(find.text('Biceps'), findsOneWidget);
 
-      expect(find.byType(ListTile), findsNWidgets(3));
+      expect(find.byType(ListTile), findsNWidgets(2));
     });
   });
 }
