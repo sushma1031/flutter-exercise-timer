8fa752bd57f09a7ee7f03004ba2dcdc3a8f184f3
sushma1031 <sushmajram@gmail.com>
2024-08-04 10:48:23 +0530

    test: update mock storage service to implement storage service API

--- test/services/mock_storage_service.dart
+++ test/services/mock_storage_service.dart
@@ -1,30 +1,84 @@
 import 'package:exercise_timer/services/storage_service.dart';
 import 'package:exercise_timer/models/exercise.dart';
 import 'package:exercise_timer/models/workout.dart';
+import 'package:exercise_timer/models/workout_display.dart';
 
 class MockStorageService extends StorageService {
-  @override
-  Future<List<Workout>> getAllWorkouts() async {
-    // have a function that returns only names of workouts
-    List<Exercise> e = [
-      new Exercise('Plank', 10),
-      new Exercise('Crunches', 5),
-    ];
+  MockStorageService(String boxName) : super(boxName);
 
-    return Future.value([
-      new Workout('Abs', e),
-      new Workout('Thighs', e),
-      new Workout('Biceps', e)
-    ]);
+  Future<void> loadData() async {
+    var e = [Exercise('Plank', 10), Exercise('Crunches', 5)];
+    workouts = [Workout('Abs', e), Workout('Thighs', e)];
   }
 
-  @override
-  Future<List<Exercise>> getWorkoutExercises(String name) async {
-    // or should I use index/id?
+  List<Workout> getAllWorkouts() {
+    return workouts;
+  }
 
-    return Future.value([
-      new Exercise('Plank', 10),
-      new Exercise('Crunches', 5),
-    ]);
+  List<WorkoutDisplay> getAllWorkoutsForDisplay() {
+    List<WorkoutDisplay> wd = [];
+    for (Workout w in workouts)
+      wd.add(WorkoutDisplay(w.name, w.exercises.length));
+    return wd;
+  }
+
+  List<Exercise> getWorkoutExercises(int index) {
+    return workouts[index].exercises;
+  }
+
+  Future<void> addOneWorkout(String name) async {
+    workouts.add(Workout(name, []));
+    return Future.value();
+  }
+
+  Future<void> addManyWorkouts(List<String> names) async {
+    for (String name in names) addOneWorkout(name);
+    return Future.value();
+  }
+
+  Future<void> updateWorkoutName(int index, String name) async {
+    Workout prev = workouts[index];
+    workouts[index] = Workout(name, prev.exercises);
+    return Future.value();
+  }
+
+  Future<void> addWorkoutExercises(int index, List<Exercise> toAdd) async {
+    Workout w = workouts[index];
+    w.exercises.addAll(toAdd);
+    return Future.value();
+  }
+
+  Future<void> updateWorkoutExercises(
+      int index, List<Exercise> newExercises) async {
+    String name = workouts[index].name;
+    workouts[index] = Workout(name, newExercises);
+    return Future.value();
+  }
+
+  Future<void> modifyWorkoutExercise(
+      int wIndex, int eIndex, String name, int duration) async {
+    Workout w = workouts[wIndex];
+    w.exercises[eIndex] = Exercise(name, duration);
+    return Future.value();
+  }
+
+  Future<void> deleteWorkout(int index) async {
+    workouts.remove(workouts[index]);
+    return Future.value();
+  }
+
+  Future<void> close() async {
+    workouts = [];
+    return Future.value();
+  }
+
+  Future<void> clear() async {
+    workouts = [];
+    return Future.value();
+  }
+
+  Future<void> delete() async {
+    workouts = [];
+    return Future.value();
   }
 }
