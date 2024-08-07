e88b218f6f1dfd6a94b3812b347e253557bbd70a
sushma1031 <sushmajram@gmail.com>
2024-08-07 15:51:57 +0530

    feat: create storage service interface
    Enables easier mocking for testing, specifically for returning ValueListenable instances for different types, i.e. Box<Workout> & List<Workout> in actual & mock services, respectively. The latter type is to avoid any dependency on Hive in the mock.

---
+++ lib/services/storage_service_interface.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import '../models/exercise.dart';
+import '../models/workout.dart';
+import '../models/workout_display.dart';
+
+abstract class StorageService<T> {
+  late T workouts;
+
+  Future<void> loadData();
+  ValueListenable<T> getListenable();
+  List<Workout> getAllWorkouts();
+  List<WorkoutDisplay> getAllWorkoutsForDisplay();
+  List<Exercise> getWorkoutExercises(int index);
+  Future<void> addOneWorkout(String name);
+  Future<void> addManyWorkouts(List<String> names);
+  Future<void> updateWorkoutName(int index, String name);
+  Future<void> addWorkoutExercises(int index, List<Exercise> toAdd);
+  Future<void> updateWorkoutExercises(int index, List<Exercise> newExercises);
+  Future<void> modifyWorkoutExercise(
+      int wIndex, int eIndex, String name, int duration);
+  Future<void> deleteWorkout(int index);
+  Future<void> close();
+  Future<void> clear();
+  Future<void> delete();
+}
