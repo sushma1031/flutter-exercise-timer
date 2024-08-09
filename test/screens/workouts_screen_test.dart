dc8d7be1378ac37b9d96de188bc633b7668e2bb4
sushma1031 <sushmajram@gmail.com>
2024-08-09 21:38:10 +0530

    test: verify adding and clearing of workouts

--- test/screens/workouts_screen_test.dart
+++ test/screens/workouts_screen_test.dart
@@ -4,7 +4,7 @@ import '../services/mock_storage_service.dart';
 import 'package:exercise_timer/main.dart';
 
 void main() {
-  var app = MyApp(db: MockStorageService('workoutBox'));
+  var app = MyApp(db: MockStorageService());
   group('Workouts Screen loads correctly', () {
     testWidgets('show "Loading..." when loading workouts', (tester) async {
       await tester.pumpWidget(app);
@@ -20,5 +20,29 @@ void main() {
 
       expect(find.byType(ListTile), findsNWidgets(2));
     });
+  });
+
+  testWidgets('adds workout correctly', (tester) async {
+    await tester.pumpWidget(app);
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byType(TextFormField), 'Biceps');
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump();
+
+    expect(find.text('Biceps'), findsOneWidget);
+
+    await tester.enterText(find.byType(TextFormField), '');
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump();
+    expect(find.text('Please enter a workout name'), findsOneWidget);
+  });
+
+  testWidgets('clears workouts correctly', (tester) async {
+    await tester.pumpWidget(app);
+    await tester.pumpAndSettle();
+    await tester.tap(find.byIcon(Icons.delete));
+    await tester.pump();
+    expect(find.byType(ListTile), findsNothing);
   });
 }
