c95bde89550bce16f28043c340d87de44dd5f669
sushma1031 <sushmajram@gmail.com>
2024-08-24 20:31:42 +0530

    feat: confirm before exiting timer on incomplete workout

--- lib/state/timer_provider.dart
+++ lib/state/timer_provider.dart
@@ -173,70 +173,108 @@ class _TimerProviderState extends State<TimerProvider>
     super.dispose();
   }
 
+  Future<bool> _onWillPop() async {
+    return await showDialog<bool>(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(
+                'Workout Incomplete!',
+                style: TextStyle(color: Colors.black),
+              ),
+              content: Text(
+                'Current workout progress will be lost. Are you sure you want to exit?',
+                style: TextStyle(color: Colors.black),
+              ),
+              actions: <Widget>[
+                TextButton(
+                    child: Text('Yes'),
+                    onPressed: () => Navigator.of(context).pop(true)),
+                TextButton(
+                    child: Text('No'),
+                    onPressed: () => Navigator.of(context).pop(false)),
+              ],
+            );
+          },
+        ) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
     // all this should go into other widgets, including ReverseCircular...
-    return SizedBox(
-        height: 300,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                  child: Stack(
-                      alignment: AlignmentDirectional.center,
-                      children: <Widget>[
-                    ReverseCircularProgressIndicator(
-                      controller: _controller,
-                    ),
-                    Center(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text('${widget.name}',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                )),
-                            Text('${formatDuration(_timeLeft)}',
-                                style: TextStyle(
-                                  fontSize: 55,
-                                  fontWeight: FontWeight.w300,
-                                ))
-                          ]),
-                    )
-                  ])),
-              Row(
+    return WillPopScope(
+        onWillPop: () {
+          setState(() {
+            _isPaused = true;
+          });
+          pauseTimer();
+          return _onWillPop();
+        },
+        child: (SizedBox(
+            height: 300,
+            child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                      tooltip: 'Previous',
-                      onPressed: goPrevious,
-                      icon: Icon(Icons.skip_previous, color: Colors.blueGrey),
-                      disabledColor: Colors.grey),
-                  IconButton(
-                      tooltip: 'Pause/Resume',
-                      onPressed: () {
-                        if (_isPaused) {
-                          resumeTimer();
-                        } else {
-                          pauseTimer();
-                        }
-                        setState(() {
-                          _isPaused = !_isPaused;
-                        });
-                      },
-                      icon: Icon((_isPaused ? Icons.play_arrow : Icons.pause),
-                          color: Colors.blueGrey)),
-                  IconButton(
-                      tooltip: 'Next',
-                      onPressed: widget.currentIndex == widget.noOfExercises
-                          ? null
-                          : goNext,
-                      icon: Icon(Icons.skip_next, color: Colors.blueGrey),
-                      disabledColor: Colors.grey)
-                ],
-              )
-            ]));
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                      child: Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: <Widget>[
+                        ReverseCircularProgressIndicator(
+                          controller: _controller,
+                        ),
+                        Center(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text('${widget.name}',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    )),
+                                Text('${formatDuration(_timeLeft)}',
+                                    style: TextStyle(
+                                      fontSize: 55,
+                                      fontWeight: FontWeight.w300,
+                                    ))
+                              ]),
+                        )
+                      ])),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                          tooltip: 'Previous',
+                          onPressed: goPrevious,
+                          icon:
+                              Icon(Icons.skip_previous, color: Colors.blueGrey),
+                          disabledColor: Colors.grey),
+                      IconButton(
+                          tooltip: 'Pause/Resume',
+                          onPressed: () {
+                            if (_isPaused) {
+                              resumeTimer();
+                            } else {
+                              pauseTimer();
+                            }
+                            setState(() {
+                              _isPaused = !_isPaused;
+                            });
+                          },
+                          icon: Icon(
+                              (_isPaused ? Icons.play_arrow : Icons.pause),
+                              color: Colors.blueGrey)),
+                      IconButton(
+                          tooltip: 'Next',
+                          onPressed: widget.currentIndex == widget.noOfExercises
+                              ? null
+                              : goNext,
+                          icon: Icon(Icons.skip_next, color: Colors.blueGrey),
+                          disabledColor: Colors.grey)
+                    ],
+                  )
+                ]))));
   }
 }
