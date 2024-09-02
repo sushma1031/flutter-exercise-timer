7832375c32c0c77fdf87f069cb207d7e14521a9f
sushma1031 <sushmajram@gmail.com>
2024-09-02 22:49:22 +0530

    ui: create widget for timer screen
    use stateless widget for display

---
+++ lib/widgets/timer_widget.dart
@@ -0,0 +1,110 @@
+import 'package:exercise_timer/utils/duration_format.dart';
+import 'package:exercise_timer/widgets/reverse_circular_progress_indicator.dart';
+import 'package:flutter/material.dart';
+
+class TimerWidget extends StatelessWidget {
+  final AnimationController controller;
+  final String name;
+  final String? nextName;
+  final Duration timeLeft;
+  final String workoutProgress;
+  final Future<void> Function() prevBtnFunction;
+  final Future<void> Function()? nextBtnFunction;
+  final void Function() togglePauseResume;
+  final IconData pauseResume;
+
+  const TimerWidget(
+      {Key? key,
+      required this.controller,
+      required this.name,
+      required this.timeLeft,
+      required this.prevBtnFunction,
+      required this.nextBtnFunction,
+      required this.togglePauseResume,
+      required this.pauseResume,
+      required this.workoutProgress,
+      this.nextName})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text(
+        (timeLeft.inSeconds > 5
+            ? ''
+            : nextName != null
+                ? 'Next Up: $nextName'
+                : 'Last one!'),
+        style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20),
+      ),
+      SizedBox(
+          height: 500,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                    child: Stack(
+                        alignment: AlignmentDirectional.center,
+                        children: <Widget>[
+                      ReverseCircularProgressIndicator(
+                        controller: controller,
+                      ),
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.only(bottom: 8),
+                                child: Text(name,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ))),
+                            Text('${formatDuration(timeLeft)}',
+                                style: TextStyle(
+                                  fontSize: 55,
+                                  fontWeight: FontWeight.w300,
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(top: 16),
+                                child: Chip(
+                                  backgroundColor:
+                                      Theme.of(context).colorScheme.background,
+                                  label: Text(
+                                    workoutProgress,
+                                    style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                      side: BorderSide(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .secondary),
+                                      borderRadius: BorderRadius.circular(5)),
+                                ))
+                          ]),
+                    ])),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        tooltip: 'Previous',
+                        onPressed: prevBtnFunction,
+                        icon: Icon(Icons.skip_previous)),
+                    IconButton(
+                        tooltip: 'Pause/Resume',
+                        onPressed: togglePauseResume,
+                        icon: Icon(pauseResume)),
+                    IconButton(
+                      tooltip: 'Next',
+                      onPressed: nextBtnFunction,
+                      icon: Icon(Icons.skip_next),
+                    )
+                  ],
+                )
+              ])),
+    ]);
+  }
+}
